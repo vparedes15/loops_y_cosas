2fb1a9c8bf0339220292427dff3cfe4c6a366f26
vparedes15 <vaparedes15@gmail.com>
2020-03-24 21:09:38 -0500

    programa_solo_suma

---
+++ sumas.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+int main (void)
+{
+  int valor1=0, valor2=0;
+  std::cout <<"Hola, ingrese dos valores\n";
+  std::cin >>valor1;
+  std::cin >>valor2;
+  std::cout <<"suma\n";
+  std::cout <<valor1+valor2<< "\n";
+  return 0;
+}
